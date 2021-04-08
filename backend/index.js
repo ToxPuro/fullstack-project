a68411814c80d986d05f8366e04a6161730a4df7
Touko Puro <toukopuro@pop-os.localdomain>
2021-04-09 00:13:16 +0300

    separated apolloserver creating from index.js

--- backend/index.js
+++ backend/index.js
@@ -1,52 +1,11 @@
-const express = require("express")
-const { ApolloServer } = require("apollo-server-express")
-const resolvers = require("./resolvers")
-const typeDefs = require("./typeDefs")
-const mongoose = require("mongoose")
-const jwt = require("jsonwebtoken")
-const User = require("./models/User")
+const createApolloServer = require('./createApolloServer')
 
-require("dotenv").config()
 
-const JWT_SECRET = process.env.JWT_SECRET
-
-async function startApolloServer() {
-
-  const MONGODB_URI = process.env.MONGODB_URI
-
-  mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-    .then(() => {
-      console.log("connected to MongoDB")
-    })
-    .catch((error) => {
-      console.log("error connection to MongoDB:", error.message)
-    })
-
-  const app = express()
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: async ({ req }) => {
-      const auth = req ? req.headers.authorization : null
-      if (auth && auth.toLowerCase().startsWith("bearer ")) {
-        const decodedToken = jwt.verify(
-          auth.substring(7), JWT_SECRET
-        )
-        const currentUser = await User.findById(decodedToken.id).populate("friends")
-        return { currentUser }
-      }
-    }
-  })
-  await server.start()
-
-  app.use(express.static("build"))
-
-  server.applyMiddleware({ app })
-
+const main = async () => {
+  const {app, server} = await createApolloServer()
   const PORT = process.env.PORT
   await new Promise(resolve => app.listen({ port: PORT }, resolve))
   console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`)
-  return { server, app }
 }
 
-startApolloServer()
+main()
