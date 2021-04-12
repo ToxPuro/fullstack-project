ef0799ec29f1643bf0a6796f4a08bbec5ef7226a
ToxPuro <puro.touko2@gmail.com>
2021-04-12 18:06:10 +0300

    If user isn't in any group gets offered link to join

--- src/components/Groups.js
+++ src/components/Groups.js
@@ -5,6 +5,14 @@ import { Link } from "react-router-dom"
 const Groups = () => {
   const groups = useQuery(USER_GROUPS)
   const displayGroups = groups.data ? groups.data.userGroups.map(group => (<li key={group.name}><Link to ={`/groups/${group.id}`}>{group.name}</Link></li>)) : []
+  if(displayGroups.length === 0 && groups.data){
+    return(
+      <div>
+        <h2>{"It seems you aren't part any group yet"}</h2>
+        <h2><Link to="/joinGroup">Join Here</Link></h2>
+      </div>
+    )
+  }
   return(
     <div>
       <ul>
