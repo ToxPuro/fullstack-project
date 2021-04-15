01dca5e4e695db62dc7b5d20872f04d0a1509ed5
ToxPuro <puro.touko2@gmail.com>
2021-04-15 11:00:46 +0300

    Changed how dates are stored

--- src/components/Event.js
+++ src/components/Event.js
@@ -5,7 +5,14 @@ import { ME, EVENT } from "../graphql/queries"
 import EventCalendar from "./EventCalendar"
 import { VOTE_EVENT } from "../graphql/mutations"
 import { Link } from "react-router-dom"
+import * as dateFns from "date-fns"
 
+const parseDate = (date) => {
+  let result = new Date(date)
+  result = dateFns.format(result, "DDD")
+  return result
+
+}
 const Event = () => {
   const [ votes, setVotes ] = useState([])
   const [ vote, response ] = useMutation(VOTE_EVENT, {
@@ -22,7 +29,7 @@ const Event = () => {
       console.log(event.data.event)
       console.log(event.data.event.dates)
       console.log(user.data.me.username)
-      let initialVotes = event.data.event.dates.map(date => ({ date: date.date, vote: "blue" }))
+      let initialVotes = event.data.event.dates.map(date => ({ date: parseDate(date.date), vote: "blue" }))
       if(event.data.event.dates[0].votes.filter(vote => vote.voter === user.data.me.username).length !== 0){
         initialVotes = event.data.event.dates.map(date => ({ date: date.date, vote: date.votes.find(vote => vote.voter === user.data.me.username ).vote }))
       }
@@ -41,7 +48,7 @@ const Event = () => {
   return(
     <div>
       {event.data.event.name}
-      <EventCalendar dates = {event.data.event.dates.map(date => date.date)} setVotes={setVotes} votes={votes}/>
+      <EventCalendar dates = {event.data.event.dates.map(date => parseDate(date.date))} setVotes={setVotes} votes={votes}/>
       <button id="voting-button" onClick={() => vote({ variables: { id: event.data.event.id, votes } })}>Vote</button>
       <button id="homepage-button"> <Link to="/">Home Page</Link></button>
     </div>
--- src/components/EventCalendar.js
+++ src/components/EventCalendar.js
@@ -4,7 +4,6 @@ import "../App.css"
 
 const EventCalendar = ({ dates, setVotes, votes }) => {
 
-
   const [ month, setMonth ] = useState(new Date())
   return(
     <div className="calendar">
@@ -100,7 +99,7 @@ const Cells = ({ month, dates, setVotes, votes }) => {
 const ChoiceDate = ({ day, formattedDate, votes, setVotes }) => {
   const [vote, setVote] = useState("")
   const cloneVotes = [...votes]
-  const dayIndex = cloneVotes.findIndex(vote => dateFns.format(vote.date, "DDD") === dateFns.format(day, "DDD"))
+  const dayIndex = cloneVotes.findIndex(vote => vote.date === dateFns.format(day, "DDD"))
   useEffect(() => {
     if(cloneVotes[dayIndex]){
       setVote(cloneVotes[dayIndex].vote)
