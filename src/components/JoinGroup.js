76226d1cacea28e50beaab19edb8cd50a25333f8
ToxPuro <puro.touko2@gmail.com>
2021-04-16 15:10:24 +0300

    removing data from cache with the better cache.modify

--- src/components/JoinGroup.js
+++ src/components/JoinGroup.js
@@ -4,9 +4,9 @@ import { GROUPS_THAT_USER_IS_NOT_IN } from "../graphql/queries"
 import { JOIN_GROUP } from "../graphql/mutations"
 import { Link } from "react-router-dom"
 import Loader from "./Loader"
-import { USER_GROUPS } from "../graphql/queries"
+import { USER_GROUPS, ME } from "../graphql/queries"
 
-const JoinGroupElement = ({ group }) => {
+const JoinGroupElement = ({ group, user }) => {
   const [ join ] = useMutation(JOIN_GROUP, {
     update: (store, response) => {
       const dataInStore = store.readQuery({ query: USER_GROUPS })
@@ -23,15 +23,13 @@ const JoinGroupElement = ({ group }) => {
           }
         })
       }
-      const groupsUserNotInData = store.readQuery({ query: GROUPS_THAT_USER_IS_NOT_IN })
-      store.writeQuery({
-        query: GROUPS_THAT_USER_IS_NOT_IN,
-        data:{
-          me: {
-            ...groupsUserNotInData.me,
-            groupsUserNotIn: groupsUserNotInData.me.groupsUserNotIn.filter(group => group.id !== response.data.joinGroup.id)
+      console.log(user.id)
+      store.modify({
+        id: `User:${user.id}`,
+        fields: {
+          groupsUserNotIn(listInCache) {
+            return listInCache.filter(group => group.__ref !== `Group:${response.data.joinGroup.id}`)
           }
-
         }
       })
     }
@@ -49,7 +47,8 @@ const JoinGroupElement = ({ group }) => {
 
 const JoinGroup = () => {
   const groups = useQuery(GROUPS_THAT_USER_IS_NOT_IN)
-  if(!groups.data){
+  const user = useQuery(ME)
+  if(!groups.data || !user.data){
     return(
       <Loader/>
     )
@@ -58,7 +57,7 @@ const JoinGroup = () => {
     <div>
       Hello World
       <ul>
-        {groups.data ? groups.data.me.groupsUserNotIn.map(group => (<JoinGroupElement key = {group.id} group={group}/>)) : null }
+        {groups.data ? groups.data.me.groupsUserNotIn.map(group => (<JoinGroupElement key = {group.id} group={group} user= {user.data.me}/>)) : null }
       </ul>
       <button id="homepage-button"> <Link to="/">Home Page</Link></button>
     </div>
