efd7d87a20f4be91cbe3c50a0b64db8e1ba34b3f
ToxPuro <puro.touko2@gmail.com>
2021-04-17 17:10:57 +0300

    different group view if user is admin

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,6 +1,15 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { USER_ID } from "../graphql/queries"
+import { useQuery } from "@apollo/client"
+import Loader from "./Loader"
 const Users = ({ users, admins }) => {
+  const userID = useQuery(USER_ID)
+  if(!userID.data){
+    return(
+      <Loader/>
+    )
+  }
   console.log("users", users)
   console.log("admins", admins)
   const adminsIDs = admins.map(admin => admin.id)
@@ -10,14 +19,15 @@ const Users = ({ users, admins }) => {
 
   return(
     <div>
-      <h2>Users</h2>
-      <ul>
-        {displayUsers}
-      </ul>
       <h2>Admins</h2>
       <ul>
         {displayAdmins}
       </ul>
+      <h2>Users</h2>
+      <ul>
+        {displayUsers}
+      </ul>
+      {adminsIDs.includes(userID.data.me.id) ? "You are admin" : null}
     </div>
   )
 }
