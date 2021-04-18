506e0ac7038449f09abf36f1a20b5b31e4e27008
ToxPuro <puro.touko2@gmail.com>
2021-04-18 20:08:07 +0300

    changed successfully

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,51 +1,19 @@
-import React, { useState, useEffect } from "react"
-import { useMutation, useQuery } from "@apollo/client"
+import React from "react"
+import {  useQuery } from "@apollo/client"
 import { useParams } from "react-router-dom"
 import { USER_EVENTS, EVENT } from "../graphql/queries"
-import { VOTE_EVENT } from "../graphql/mutations"
 import PickingEvent from "./PickingEvent"
 import DoneEvent from "./DoneEvent"
 import VotingEvent from "./VotingEvent"
 
-const parseDate = (date) => {
-  console.log(date)
-  let result = new Date(date)
-  return result
 
-}
 const Event = ({ setNotification }) => {
-  const [ votes, setVotes ] = useState([])
-  const [ vote, response ] = useMutation(VOTE_EVENT, {
-    onError : (error) => {
-      console.log(error)
-    }
-  })
-  const onClick = async () => {
-    await vote({ variables: { id: event.data.event.id, votes } })
-    setNotification({ message: "Voted successfully", error: false })
-    setTimeout(() => {
-      setNotification(null)
-    }, 5000)
-  }
-  console.log(response)
   const id = useParams().id
   const user= useQuery(USER_EVENTS)
   console.log(user)
   const event = useQuery(EVENT, { variables: { id } })
   console.log(event)
-  useEffect(() => {
-    if(event.data && user.data){
-      console.log(event.data.event)
-      console.log(event.data.event.dates)
-      console.log("User", user.data.me.username)
-      let initialVotes = event.data.event.dates.map(date => ({ date: parseDate(date.date), vote: "blue" }))
-      if(event.data.event.dates[0].votes.filter(vote => vote.voter === user.data.me.username).length !== 0){
-        console.log("dates", event.data.event.dates)
-        initialVotes = event.data.event.dates.map(date => ({ date: parseDate(date.date), vote: date.votes.find(vote => vote.voter === user.data.me.username ).vote }))
-      }
-      setVotes(initialVotes)
-    }
-  },[event.data, user.data])
+
 
   if(!event.data || !user.data){
     return(
@@ -70,7 +38,7 @@ const Event = ({ setNotification }) => {
   }
   return(
     <div>
-      <PickingEvent user={user.data.me} event={event.data.event} />
+      <PickingEvent user={user.data.me} event={event.data.event} setNotification={setNotification}/>
     </div>
   )
 }
--- src/components/PickingEvent.js
+++ src/components/PickingEvent.js
@@ -1,7 +1,39 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import { useMutation } from "@apollo/client"
+import { VOTE_EVENT } from "../graphql/mutations"
 import EventCalendar from "./EventCalendar"
+import { Link } from "react-router-dom"
 
-const PickingEvent = ({ event, user }) => {
+const parseDate = (date) => {
+  console.log(date)
+  let result = new Date(date)
+  return result
+
+}
+
+const PickingEvent = ({ event, user, setNotification }) => {
+  const [ votes, setVotes ] = useState([])
+  useEffect(() => {
+    if(event && user ){
+      let initialVotes = event.dates.map(date => ({ date: parseDate(date.date), vote: "blue" }))
+      if(event.dates[0].votes.filter(vote => vote.voter === user.username).length !== 0){
+        initialVotes = event.dates.map(date => ({ date: parseDate(date.date), vote: date.votes.find(vote => vote.voter === user.username ).vote }))
+      }
+      setVotes(initialVotes)
+    }
+  },[event, user])
+  const [ vote ] = useMutation(VOTE_EVENT, {
+    onError : (error) => {
+      console.log(error)
+    }
+  })
+  const onClick = async () => {
+    await vote({ variables: { id: event.id, votes } })
+    setNotification({ message: "Voted successfully", error: false })
+    setTimeout(() => {
+      setNotification(null)
+    }, 5000)
+  }
   return(
     <div>
       <EventCalendar dates = {event.dates.map(date => parseDate(date.date))} setVotes={setVotes} votes={votes}/>
