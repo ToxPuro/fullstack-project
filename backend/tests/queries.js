1d137df40a39ee2fe07b66b22c41b768c99f5a71
ToxPuro <puro.touko2@gmail.com>
2021-04-23 13:27:59 +0300

    resolver doesn't pass test

--- backend/tests/queries.js
+++ backend/tests/queries.js
@@ -91,4 +91,9 @@ mutation removeFromGroup($group: String!, $user: String!){
   removeFromGroup(group: $group, user: $user){name}
 }`
 
-module.exports = { ADD_EVENT, LOGIN, ADD_GROUP, ME, USER_GROUPS, USER_EVENTS, GET_EVENT, VOTE_EVENT, JOIN_GROUP, GROUPS_THAT_USER_IS_NOT_IN, LEAVE_GROUP, DELETE_EVENT, USER_MESSAGES, ADD_TO_ADMINS, REMOVE_FROM_GROUP }
+const JOIN_REQUEST = gql`
+mutation joinRequest($group: String!, $content: String!){
+  joinRequest(group: $group, content: $content){name}
+}`
+
+module.exports = { ADD_EVENT, LOGIN, ADD_GROUP, ME, USER_GROUPS, USER_EVENTS, GET_EVENT, VOTE_EVENT, JOIN_GROUP, GROUPS_THAT_USER_IS_NOT_IN, LEAVE_GROUP, DELETE_EVENT, USER_MESSAGES, ADD_TO_ADMINS, REMOVE_FROM_GROUP, JOIN_REQUEST }
