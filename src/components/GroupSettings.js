19e06f4f00ac9742c68f364cddc2e5c8865fcb52
ToxPuro <puro.touko2@gmail.com>
2021-04-27 18:17:22 +0300

    selector for changing privacy type

--- src/components/GroupSettings.js
+++ src/components/GroupSettings.js
@@ -1,119 +1,27 @@
-import React from "react"
-import { Formik } from "formik"
-import * as yup from "yup"
-import axios from "axios"
+import React, { useState } from "react"
+import { Link } from "react-router-dom"
+import Select from "react-select"
 
-const validationSchema = yup.object().shape({
-  username: yup
-    .string()
-    .required("Username is required")
-    .min(3),
-  name: yup
-    .string()
-    .required("Name is required")
-    .min(3),
-  password: yup
-    .string()
-    .required("Password is required")
-    .min(8)
+const options = [{ value: "private", label: "private" }, { value: "open", label: "open" }, { value: "public", label: "public" }]
 
-})
-
-const SignIn = ({ login, signIn }) => {
-  const onSubmit = async ({ username, name, password, image }, { resetForm }) => {
-    let avatarID = null
-    if(image){
-      const formData = new FormData()
-      formData.append("file", image)
-      formData.append("upload_preset", "ml_default")
-      const response = await axios.post(
-        "https://api.cloudinary.com/v1_1/dfayht8i9/image/upload",
-        formData,
-      )
-      avatarID = response.data.public_id
-      console.log(avatarID)
-    }
-    const signInSuccessful = await signIn(username, name, password, avatarID)
-    console.log(signInSuccessful)
-    if(signInSuccessful){
-      await login({ username, password })
-    }
-
-    resetForm({ values: { username: "", password: "", name: "", image: null } })
+const AddGroup = ({ setNotification }) => {
+  const [privacyOption, setPrivacyOption ] = useState("private")
+  console.log(privacyOption)
+  const handleChoice = selectedOption => setPrivacyOption(selectedOption.value)
+  const Change = () => {
+    setNotification({ message: "Changes Saved", error: false })
+    setTimeout(() => {
+      setNotification(null)
+    }, 5000)
   }
-  console.log("yeet")
-
-
-
-
-
   return(
     <div>
-      <h1>Sign In</h1>
-      <Formik
-        initialValues={{ username: "", password: "", name: "", image: null }}
-        onSubmit={onSubmit}
-        validationSchema = {validationSchema}
-      >
-        {({
-          values,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          setFieldValue,
-          isValid,
-          /* and other goodies */
-        }) => (
-          <form onSubmit={handleSubmit}>
-           username: <input
-              id="username"
-              type="text"
-              name="username"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.username}
-            />
-
-            <br/>
-
-            name: <input
-              id="name"
-              type="text"
-              name="name"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.name}
-            />
-
-            <br/>
-
-           password: <input
-              id="password"
-              type= "text"
-              name="password"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.password}
-            />
-
-            <br/>
-
-          image: <input
-              id="image"
-              type="file"
-              name="image"
-              onChange={(event) => {
-                setFieldValue("image", event.currentTarget.files[0])
-              }}/>
-            <br/>
-            { (isValid) ?             <button type="submit" id="submit-button">
-             Sign In
-            </button> : null}
-          </form>
-        )}
-      </Formik>
+      <h1>Group Settings</h1>
+      <Select id="group-privacy" options={options} onChange={handleChoice}/>
+      <button onClick={Change}>Save Changes </button>
+      <button id="homepage-button"> <Link to="/">Home Page</Link></button>
     </div>
   )}
 
 
-export default SignIn
+export default AddGroup
