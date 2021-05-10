85579edeb79c890f3fc871716bbcb0cad5a23379
ToxPuro <puro.touko2@gmail.com>
2021-05-10 16:28:47 +0300

    docker

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node:15.12.0
+
+WORKDIR /usr/src/app
+
+COPY . .
+
+RUN npm install
+
+EXPOSE 4000
+
+CMD ["npm", "run", "start-server"]
--- backend/models/GroupMessage.js
+++ backend/models/GroupMessage.js
@@ -2,16 +2,9 @@ const mongoose = require("mongoose")
 
 
 const groupMessageSchema = new mongoose.Schema({
-  title: String,
   content: String,
   read: Boolean,
-  type: String,
   sender: String,
-  group: String,
-  receivers:{
-    type: [String],
-    default: []
-  }
 })
 
 
